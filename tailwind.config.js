7d9ae3d36b1469045f6dd165b044aa3f38e30269
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-03-22 16:03:17 +0100

    Kaiserstuhl colors added to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ks-orange': "rgb(255 174 0 / <alpha-value>)",
+        'ks-green': "rgb(55 179 74 / <alpha-value>)",
+        'ks-black': "rgb(10 10 10 / <alpha-value>)",
+        'ks-grey': "rgb(10 10 10 / <alpha-value>)",
+        'ks-white': '#rgb(69 66 65 / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
