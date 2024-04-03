5792e5893d217e8769cfdb956124604077a0c6fe
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-04-03 10:55:27 +0200

    Images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
       colors: {
         'ks-orange': "rgb(255 174 0 / <alpha-value>)",
         'ks-green': "rgb(55 179 74 / <alpha-value>)",
-        'ks-black': "rgb(10 10 10 / <alpha-value>)",
-        'ks-grey': "rgb(10 10 10 / <alpha-value>)",
-        'ks-white': '#rgb(69 66 65 / <alpha-value>)',
+        'ks-black': "rgb(26 25 25 / <alpha-value>)",
+        'ks-grey': "rgb(69 66 65 / <alpha-value>)",
+        'ks-white': 'rgb(255 255 255 / <alpha-value>)',
       },
     },
   },
