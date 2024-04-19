29b9b6e37c35d414060b7d11872cd5a377bc63cf
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-04-19 10:41:43 +0200

    before PR

--- public/vue/vueAccueil.php
+++ public/vue/vueAccueil.php
@@ -4,89 +4,92 @@ $message = "";
 ?>
 
 <body class="overflow-x-hidden bg-ks-black">
-<section class="h-[60vh] md:h-[100vh] bg-cover bg-no-repeat" style="background-image: url('img/ks-bg1.png');">
+    <section class="h-[60vh] md:h-[100vh] bg-cover bg-no-repeat" style="background-image: url('img/ks-bg1.png');">
 
-<h1 class="text-white font-semibold mt-0 pt-40 text-4xl md:text-6xl text-center my-40 select-none">VOTRE <span
-        class="text-ks-orange">AVENTURE</span><br><span class="text-ks-orange">EN</span> EXTERIEUR
-</h1>
-</section>
+        <h1 class="text-white font-semibold mt-0 pt-40 text-4xl md:text-6xl text-center my-40 select-none">VOTRE <span
+                class="text-ks-orange">AVENTURE</span><br><span class="text-ks-orange">EN</span> EXTERIEUR
+        </h1>
+    </section>
 
-<section class="relative h-[100vh] mb-2 bg-ks-black">
-<div class="flex flex-col items-center">
-    <div class="flex flex-col">
-        <P class="text-ks-white ">Défier l'inconnu, c'est ouvrir la porte vers l'extraordinaire, mais ce qui
-            se cache derrière pourrait bien dépasser toute attente...</P>
-        <button class="bg-ks-orange w-fit p-2 rounded">Découvrir nos jeux</button>
+    <section class="relative h-[100vh] mb-2 bg-ks-black">
+        <div class="flex flex-col items-center">
+            <div class="flex flex-col">
+                <P class="text-ks-white ">Défier l'inconnu, c'est ouvrir la porte vers l'extraordinaire, mais ce qui
+                    se cache derrière pourrait bien dépasser toute attente...</P>
+                <button class="bg-ks-orange w-fit p-2 rounded">Découvrir nos jeux</button>
 
-    </div>
-    <img class="absolute translate-x-[100px]" src="img/image-removebg-preview (10) 1.png" alt="">
-</div>
-</section>
+            </div>
+            <img class="absolute translate-x-[100px]" src="img/image-removebg-preview (10) 1.png" alt="">
+        </div>
+    </section>
 
-<section class=" bg-ks-black">
-<p>L'EVENEMENT D'EQUIPE PARFAIT AVEC 100% DE PLAISIR GARANTI POUR LES ENTREPRISES, LES SSOCIATIONS, LA FAMILLE ET
-    LES AMIS..</p>
-<div> <!-- Leaderboard --> </div>
+    <section class=" bg-ks-black">
+        <p>L'EVENEMENT D'EQUIPE PARFAIT AVEC 100% DE PLAISIR GARANTI POUR LES ENTREPRISES, LES SSOCIATIONS, LA FAMILLE
+            ET
+            LES AMIS..</p>
+        <div> <!-- Leaderboard --> </div>
 
-</section>
+    </section>
 
-<section class=" bg-ks-black">
-<h2>QUI SOMMES-<span>NOUS</span></h2>
+    <section class=" bg-ks-black">
+        <h2>QUI SOMMES-<span>NOUS</span></h2>
 
-<div class="flex">
-<div class="bg-ks-orange w-fit flex flex-col justify-center gap-2 p-5">
-       
-       <img class="w-30 lg:w-40" src="img/icons/facebook.svg" alt="">
-       <img class="w-30 lg:w-40" src="img/icons/instagram.svg" alt="">
-       <img class="w-30 lg:w-40" src="img/icons/youtube.svg" alt="">
-          
-   </div>
-   
-    <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.</p>
-        <button class="bg-ks-orange">En savoir plus..</button>
-    </div>
-</div>
-</section>
+        <div class="flex">
+            <div class="bg-ks-orange w-fit flex flex-col justify-center gap-2 p-5">
 
-<section
-class="h-[20vh] bg-ks-grey px-4 text-xs md:px-64 md:py-6 md:text-lg text-ks-white flex justify-between align-middle items-center">
-<div class="flex flex-col align-middle items-center text-center gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2"
-        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-        <circle cx="12" cy="12" r="10" />
-        <path d="M16.2 7.8l-2 6.3-6.4 2.1 2-6.3z" />
-    </svg>
-    <p>L'AVENTURE</p>
-</div>
-<div class="flex flex-col align-middle items-center text-center gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2"
-        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-        <circle cx="9" cy="7" r="4"></circle>
-        <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-        <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-    </svg>
-    <p>TEAM BUILDING</p>
-</div>
-<div class="flex flex-col align-middle items-center text-center gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2"
-        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-        <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
-    </svg>
-    <p>MYSTERES</p>
-</div>
-</section>
+                <img class="w-30 lg:w-40" src="img/icons/facebook.svg" alt="">
+                <img class="w-30 lg:w-40" src="img/icons/instagram.svg" alt="">
+                <img class="w-30 lg:w-40" src="img/icons/youtube.svg" alt="">
 
-<section class="h-[100vh] bg-ks-black">
+            </div>
 
-</section>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+                <button class="bg-ks-orange">En savoir plus..</button>
+            </div>
+        </div>
+    </section>
 
-<script src="js/script.js"></script>
+    <section
+        class="h-[20vh] bg-ks-grey px-4 text-xs md:px-64 md:py-6 md:text-lg text-ks-white flex justify-between align-middle items-center">
+        <div class="flex flex-col align-middle items-center text-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none"
+                stroke="#f2f2f2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="10" />
+                <path d="M16.2 7.8l-2 6.3-6.4 2.1 2-6.3z" />
+            </svg>
+            <p>L'AVENTURE</p>
+        </div>
+        <div class="flex flex-col align-middle items-center text-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none"
+                stroke="#f2f2f2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                <circle cx="9" cy="7" r="4"></circle>
+                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+            </svg>
+            <p>TEAM BUILDING</p>
+        </div>
+        <div class="flex flex-col align-middle items-center text-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none"
+                stroke="#f2f2f2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
+            </svg>
+            <p>MYSTERES</p>
+        </div>
+    </section>
+
+    <section class="h-[100vh] bg-ks-black">
+
+    </section>
+
+    <script src="js/script.js"></script>
 </body>
-
